1d32360860331097118ee0c282c56ca362afe77b
8mgofprotein <122740216+8mgofprotein@users.noreply.github.com>
2023-07-24 20:19:13 +0700

    Initial commit

---
+++ lua/autorun/bapmsdpack.lua
@@ -0,0 +1,3 @@
+--Add Playermodel
+player_manager.AddValidModel( "Blue Archive - Neru (Original)", "models/kyo/neru_pm.mdl" )
+player_manager.AddValidHands( "Blue Archive - Neru (Original)", "models/kyo/c_arms_neru_sd.mdl", 0, "00000000" )
---
+++ lua/autorun/bapmsdpacknpc.lua
@@ -0,0 +1,29 @@
+--Add NPC
+local NPC =
+{
+	Name = "Neru (Friendly)",
+	Class = "npc_citizen",
+	KeyValues = { citizentype = 4, spawnflags = 155908, },
+	Model = "models/kyo/neru_npc_citizen.mdl",
+	Health = "200",
+	Weapons = { "weapon_smg" },
+	Category = "Nekyoko's Blue Archive NPC"
+	
+}
+
+list.Set( "NPC", "npc_neru_kyo_f", NPC )
+
+local NPC =
+{
+	Name = "Neru (Hostile)",
+	Class = "npc_combine_s",
+	Model = "models/kyo/neru_npc_combine.mdl",
+	Health = "200",
+	Squadname = "PLAGUE",
+	Numgrenades = "15",
+	Weapons = { "weapon_smg" },
+	Category = "Nekyoko's Blue Archive NPC"
+	
+}
+
+list.Set( "NPC", "npc_neru_kyo_h", NPC )
