208d6063a9bd63789e0d0707ea767d68f4cd8937
8mgofprotein <122740216+8mgofprotein@users.noreply.github.com>
2023-07-25 01:16:17 +0700

    Azusa Added

--- lua/autorun/bapmsdpack.lua
+++ lua/autorun/bapmsdpack.lua
@@ -1,3 +1,6 @@
---Add Playermodel
+--Neru
 player_manager.AddValidModel( "Blue Archive - Neru (Original)", "models/kyo/neru_pm.mdl" )
 player_manager.AddValidHands( "Blue Archive - Neru (Original)", "models/kyo/c_arms_neru_sd.mdl", 0, "00000000" )
+--Azusa
+player_manager.AddValidModel( "Blue Archive - Azusa (Original)", "models/kyo/azusa_pm.mdl" )
+player_manager.AddValidHands( "Blue Archive - Azusa (Original)", "models/kyo/c_arms_azusa_sd.mdl", 0, "00000000" )
--- lua/autorun/bapmsdpacknpc.lua
+++ lua/autorun/bapmsdpacknpc.lua
@@ -1,4 +1,4 @@
---Add NPC
+--Neru
 local NPC =
 {
 	Name = "Neru (Friendly)",
@@ -27,3 +27,34 @@ local NPC =
 }
 
 list.Set( "NPC", "npc_neru_kyo_h", NPC )
+
+local NPC =
+{
+	Name = "Azusa (Friendly)",
+	Class = "npc_citizen",
+	KeyValues = { citizentype = 4, spawnflags = 155908, },
+	Model = "models/kyo/azusa_npc_citizen.mdl",
+	Health = "200",
+	Weapons = { "weapon_smg" },
+	Category = "Nekyoko's Blue Archive NPC"
+	
+}
+
+--Azusa
+
+list.Set( "NPC", "npc_azusa_kyo_f", NPC )
+
+local NPC =
+{
+	Name = "Azusa (Hostile)",
+	Class = "npc_combine_s",
+	Model = "models/kyo/azusa_npc_combine.mdl",
+	Health = "200",
+	Squadname = "PLAGUE",
+	Numgrenades = "15",
+	Weapons = { "weapon_smg" },
+	Category = "Nekyoko's Blue Archive NPC"
+	
+}
+
+list.Set( "NPC", "npc_azusa_kyo_h", NPC )
